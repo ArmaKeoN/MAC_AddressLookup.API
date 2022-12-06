d793871bc9b4c8a276044ecbb6760abff294adc3
Jonah Lozano <jonah.lozano03@gmail.com>
2022-12-06 17:44:10 -0600

    Forgot to stage all files for init commit

--- MAC_AddressLookup.API/Program.cs
+++ MAC_AddressLookup.API/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using MAC_AddressLookup.API;
+using Newtonsoft.Json;
+using System.Text.Json;
 
+Console.WriteLine("Please proivde a MAC address: ");
+
+string? macAddress = Console.ReadLine();
+
+string url = $"https://api.macaddress.io/v1?apiKey=at_7rligFVhix4xCZ5GR68QQ5C6MdVvB&output=json&search={macAddress}";
+
+using var client = new HttpClient();
+client.DefaultRequestHeaders.Accept.Add(
+   new MediaTypeWithQualityHeaderValue("application/json"));
+
+var response = client.GetAsync(url).Result;
+Root? root = new Root();
+
+if (response.IsSuccessStatusCode)
+{
+    string jsonResult = await response.Content.ReadAsStringAsync();
+    root = JsonConvert.DeserializeObject<Root>(jsonResult);
+}
+else
+{
+    Console.WriteLine("{0} ({1})", (int)response.StatusCode,
+                  response.ReasonPhrase);
+}
+
+if(root.macAddressDetails.isValid)
+{
+    Console.WriteLine("The provided MAC Address to search for is valid and here and the following details:");
+    Console.WriteLine("Company Name: " + root.vendorDetails.companyName);
+    Console.WriteLine("MAC Address: " + root.macAddressDetails.searchTerm);
+    Console.ReadLine();
+}
